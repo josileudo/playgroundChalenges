9ca386d3b1010d61c4f103afa425e759c66c70dd
Josileudo <josileudorodrigues75@gmail.com>
2022-11-05 12:05:46 -0300

    :hammer: Add constant with limit string

--- Contents.swift
+++ Contents.swift
@@ -4,7 +4,8 @@
 var myAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "w", "y", "z"];
 
 var myAlphabetNew = myAlphabet.joined();
+var maxString = 6
 
-let myPassword = String((0..<6).compactMap{_ in myAlphabetNew.randomElement() });
+let myPassword = String((0..<maxString).compactMap{_ in myAlphabetNew.randomElement() });
 
 print(myPassword);
